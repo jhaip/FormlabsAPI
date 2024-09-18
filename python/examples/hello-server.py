54fbabb62cceb37990d2490cfddd404f3f485cf6
Jacob Haip <jhaip@users.noreply.github.com>
2024-09-18 16:41:56 -0400

    update hello and batching py demos, add web sls packing demo

--- python/examples/hello-server.py
+++ python/examples/hello-server.py
@@ -1,15 +1,24 @@
 import formlabs
+from formlabs import SceneTypeModel, SceneTypeModelLayerThickness
 import pathlib
+import sys
 
 def hello_server():
-    pathToPreformServer = pathlib.Path().resolve() / "PreFormServer.app/Contents/MacOS/PreFormServer"
+    pathToPreformServer = None
+    if sys.platform == 'win32':
+        pathToPreformServer = pathlib.Path().resolve() / "PreFormServer.exe"
+    elif sys.platform == 'darwin':
+        pathToPreformServer = pathlib.Path().resolve() / "PreFormServer.app/Contents/MacOS/PreFormServer"
+    else:
+        print("Unsupported platform")
+        sys.exit(1)
     with formlabs.PreFormApi.start_preform_server(pathToPreformServer=pathToPreformServer) as preform:
-        preform.api.scene_post({
-            "machine_type": "FORM-4-0",
-            "material_code": "FLRG1011",
-            "slice_thickness": 0.1,
-            "print_setting": "DEFAULT",
-        })
+        preform.api.scene_post(SceneTypeModel(
+            machine_type="FORM-4-0",
+            material_code="FLRG1011",
+            layer_thickness=SceneTypeModelLayerThickness("0.1"),
+            print_setting="DEFAULT",
+        ))
         print("Scene created, exiting")
 
 if __name__ == "__main__":
